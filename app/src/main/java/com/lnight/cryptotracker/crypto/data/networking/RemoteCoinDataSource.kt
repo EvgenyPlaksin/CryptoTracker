65e8a2a025211b7847de469527140197d0beae1e
Lnight <plaksinevgeniy766@gmail.com>
2024-11-10 16:26:19 +0100

    create CoinListViewModel.kt

--- app/src/main/java/com/lnight/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/lnight/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
@@ -11,19 +11,23 @@ import com.lnight.cryptotracker.crypto.domain.Coin
 import com.lnight.cryptotracker.crypto.domain.CoinDataSource
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class RemoteCoinDataSource(
     private val httpClient: HttpClient
 ): CoinDataSource {
 
     override suspend fun getCoins(): Result<List<Coin>, NetworkError> {
-        return safeCall<CoinsResponseDto> {
-            httpClient.get(
-                urlString = constructUrl("/assets")
+        return withContext(Dispatchers.IO) {
+            safeCall<CoinsResponseDto> {
+                httpClient.get(
+                    urlString = constructUrl("/assets")
 
-            )
-        }.map { response ->
-            response.data.map { it.toCoin() }
+                )
+            }.map { response ->
+                response.data.map { it.toCoin() }
+            }
         }
     }
 }
